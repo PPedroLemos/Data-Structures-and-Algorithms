9ea6626e0a50e7c2054be1eb1b2a2e368d3748e6
PedroLemos <pedro.comp1210@gmail.com>
2026-09-22 17:02:44 -0300

    Stack overhaul

--- include/pdsa/data_structures/stack.hpp
+++ include/pdsa/data_structures/stack.hpp
@@ -1,23 +1,28 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include <cstddef>
+#include "./vector.hpp"
+
 namespace pdsa
 {
 
-template<typename T> class stack
+template <typename T, typename Container = pdsa::vector<T>> class Stack
 {
-private:
-    T* data_;
-    std::size_t size_;
-    std::size_t capacity_;
-public:
-    std::size_t size() const { return size_; }
-    std::size_t capacity() const { return capacity_; }
-    bool empty() const  { return size_ == 0; }
+  private:
+    Container container;
 
+  public:
+    Stack() : container() {}
+
+    std::size_t size() const { return container.size(); }
+    std::size_t capacity() const { return container.capacity(); }
+    bool empty() const { return container.empty(); }
+
+    void push(const T& value) { container.push_back(value); }
+    T& top() { container.back(); }
+    T& pop() { container.pop_back(); }
 };
 
-}
+} // namespace pdsa
 
 #endif
