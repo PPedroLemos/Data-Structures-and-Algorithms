d35b3741e6f0a796400f31e330cfb5689a515c74
PedroLemos <pedro.comp1210@gmail.com>
2026-09-22 11:06:19 -0300

    Add exception safety to vector constructors

--- include/pdsa/data_structures/vector.hpp
+++ include/pdsa/data_structures/vector.hpp
@@ -45,21 +45,44 @@ template <typename T> class vector
     vector(std::size_t count, const T& value) : data_(nullptr), size_(0), capacity_(0)
     {
         if (count == 0) return;
-        size_ = count;
         capacity_ = 1;
         while (capacity_ < count) capacity_ *= 2;
         data_ = alloc_traits::allocate(alloc, capacity_);
-        for (std::size_t i = 0; i < count; i++) alloc_traits::construct(alloc, data_ + i, value);
+        try
+        {
+            for (std::size_t i = 0; i < count; i++)
+            {
+                alloc_traits::construct(alloc, data_ + size_, value);
+                ++size_;
+            }
+        }
+        catch (...)
+        {
+            for (std::size_t i = 0; i < size_; i++) alloc_traits::destroy(alloc, data_ + i);
+            alloc_traits::deallocate(alloc, data_, capacity_);
+            throw;
+        }
     }
-    vector(std::initializer_list<T> initializer_list)
-        : data_(nullptr), size_(0), capacity_(0)
+    vector(std::initializer_list<T> initializer_list) : data_(nullptr), size_(0), capacity_(0)
     {
         if (initializer_list.size() == 0) return;
         capacity_ = 1;
         while (capacity_ < initializer_list.size()) capacity_ *= 2;
         data_ = alloc_traits::allocate(alloc, capacity_);
-        for (const T& value : initializer_list)
-            alloc_traits::construct(alloc, data_ + size_++, value);
+        try
+        {
+            for (const T& value : initializer_list)
+            {
+                alloc_traits::construct(alloc, data_ + size_, value);
+                ++size_;
+            }
+        }
+        catch (...)
+        {
+            for (std::size_t i = 0; i < size_; i++) alloc_traits::destroy(alloc, data_ + i);
+            alloc_traits::deallocate(alloc, data_, capacity_);
+            throw;
+        }
     }
     ~vector()
     {
